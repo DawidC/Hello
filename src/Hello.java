94c8cd30e1691e92a8099767c22049b10987e07a
DawidC <dawid.code+git@gmail.com>
2015-07-24 11:17:22 +0200

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+/**
+ * Created by dawid on 24.07.15.
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello gitjava !!");ls
+                
+    }
+}
